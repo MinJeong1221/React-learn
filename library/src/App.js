283985cc14eca68b03f8e6d90e72886b410bc38e
MinJeong1221 <mjeong1998@naver.com>
2023-07-13 00:41:45 +0900

    eslint prettier 설치와 설정

--- library/src/App.js
+++ library/src/App.js
@@ -1,15 +1,16 @@
+import React from 'react';
 import './App.css';
 import Moment from './3-1/Moment';
 import Day from './3-1/Day';
 
-
-
 function App() {
   return (
-    <div className="App">
-      <Moment />
-      <Day />
-    </div>
+    <>
+      <div className="App">
+        <Moment />
+        <Day />
+      </div>
+    </>
   );
 }
 
